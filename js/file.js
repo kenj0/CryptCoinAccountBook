e3a905293b9d4fd0700eee44f0f1fd62e5358914
take-iwiw <take.iwiw2222@gmail.com>
2017-12-22 18:27:37 +0900

    first commit

---
+++ js/file.js
@@ -0,0 +1,20 @@
+function loadHistory(e, callback) {
+	var selectedFile = e.target.files[0];
+	var reader = new FileReader();
+	reader.readAsText(selectedFile);
+	reader.addEventListener('load', function() {
+		// console.log(reader.result);
+		g_history = JSON.parse(reader.result)
+		// console.log(g_history);
+		callback();
+	})
+}
+
+function saveHistory() {
+	var jsonString = JSON.stringify(g_history);
+	var blob = new Blob([jsonString],{type:'text/plain'});
+	window.URL = window.URL || window.webkitURL;
+	var saveElem = document.getElementById("btn-history-save");
+	saveElem.setAttribute("href", window.URL.createObjectURL(blob));
+	saveElem.setAttribute("download", "History_" + new Date());
+}
