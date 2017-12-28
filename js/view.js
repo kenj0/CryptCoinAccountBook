9fc40881992523b9248b11879507723a964be005
kenj0 <32511018+kenj0@users.noreply.github.com>
2017-12-28 23:44:06 +0900

    Modify tab to space. Delete double "BCH" declaration from coin list.

--- js/view.js
+++ js/view.js
@@ -14,93 +14,93 @@ var g_historyStr = null;
 var g_usedCoinList = null
 
 /* リスト表示の候補。g_coinListはユーザが新しいコインを自分で追加したら、動的に増える */
-var g_coinList = ["JPY", "BTC", "BCH", "ETH", "XRP", "LTC", "XMR", "BCH", "IOT", "DASH", "ADA", "NEO", "EOS", "XLM", "QTUM", "ETC", "LSK", "ZEC", "XVG", "SC", "DOGE", "WAVES", "GNT", "SNT", "REP", "STRAT", "BTS", "XEM", "DGB", "BTG", "GAS", "SBD", "VTC", "NXT", "FCT", "RDD", "XZC", "STEEM", "PIVX", "OMG", "KMD", "LBC", "STORJ", "MONA", "ZENY", "XP", "TRON"];
+var g_coinList = ["JPY", "BTC", "BCH", "ETH", "XRP", "LTC", "XMR", "IOT", "DASH", "ADA", "NEO", "EOS", "XLM", "QTUM", "ETC", "LSK", "ZEC", "XVG", "SC", "DOGE", "WAVES", "GNT", "SNT", "REP", "STRAT", "BTS", "XEM", "DGB", "BTG", "GAS", "SBD", "VTC", "NXT", "FCT", "RDD", "XZC", "STEEM", "PIVX", "OMG", "KMD", "LBC", "STORJ", "MONA", "ZENY", "XP", "TRON"];
 var g_marketplaceList = ["", "bitFlyer", "Zaif", "coincheck", "bitbank", "QUOINEX", "GMO", "POLONIEX", "BITTREX", "Liqui", "Cryptopia", "HitBTC", "CCEX", "Binance", "CryptoBridge", "CoinExchange"];
 
 /*** AngularJS ***/
 /* g_historyStrを基にテーブル表示をするためだけにAngularJSを使う。 ひとまず、他の処理はAngularJS外とする。*/
 /* そのため、AngularJS外の処理からテーブルの更新処理を読み出すために、updateHistoryDummyButton要素を経由している。これはもっとうまく実装できるはず。*/
-var app = angular.module("myApp", []); 
-app.controller("myCtrl", function($scope) { 
-	$scope.updateHistory = function() {
-		$scope.history = g_historyStr;
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+  $scope.updateHistory = function() {
+    $scope.history = g_historyStr;
 
-		var portfolio = [];
-		g_usedCoinList.forEach(function(coin) {
-			if(coin != "JPY") {
-				portfolio.push({
-					"coin": coin,
-					"balance": g_historyCalc[g_historyCalc.length - 1]["balance_" + coin].toLocaleString(),
-					"value": g_historyCalc[g_historyCalc.length - 1]["value_" + coin].toLocaleString(),
-					"averageAcquisitionPrice": g_historyCalc[g_historyCalc.length - 1]["averageAcquisitionPrice_" + coin].toLocaleString(),
-				});
-			}
-		});
-		$scope.portfolio = portfolio;
-	};
+    var portfolio = [];
+    g_usedCoinList.forEach(function(coin) {
+      if(coin != "JPY") {
+        portfolio.push({
+          "coin": coin,
+          "balance": g_historyCalc[g_historyCalc.length - 1]["balance_" + coin].toLocaleString(),
+          "value": g_historyCalc[g_historyCalc.length - 1]["value_" + coin].toLocaleString(),
+          "averageAcquisitionPrice": g_historyCalc[g_historyCalc.length - 1]["averageAcquisitionPrice_" + coin].toLocaleString(),
+        });
+      }
+    });
+    $scope.portfolio = portfolio;
+  };
 });
 
 
 /*** イベントハンドラーの登録 ***/
 /* 「取引履歴の管理」関連 */
 $(function() {
-	if(window.File && window.FileReader && window.FileList && window.Blob) {
-		$('#file-history-load').on('change', function (e) {
-			loadHistory(e, calcHistory);
-		});
+  if(window.File && window.FileReader && window.FileList && window.Blob) {
+    $('#file-history-load').on('change', function (e) {
+      loadHistory(e, calcHistory);
+    });
 
-		$('#btn-history-save').on('click', function () {
-			saveHistory();
-		});
+    $('#btn-history-save').on('click', function () {
+      saveHistory();
+    });
 
-		$('#btn-history-clear').on('click', function () {
-			clearHistory();
-			calcHistory();
-			$('#btn-update-history').click();
-		});
+    $('#btn-history-clear').on('click', function () {
+      clearHistory();
+      calcHistory();
+      $('#btn-update-history').click();
+    });
 
-		$('#btn-profit').on('click', function () {
-			calcProfit();
-		});
-	} else {
-		file.style.display = 'none';
-		alert("ブラウザからのファイル制御を有効にしてください")
-	}
+    $('#btn-profit').on('click', function () {
+      calcProfit();
+    });
+  } else {
+    file.style.display = 'none';
+    alert("ブラウザからのファイル制御を有効にしてください")
+  }
 });
 
 /* 「取引の入力」関連 */
 $(function() {
-	$('#flag-trans-isAltAlt').on('change', function () {
-		if ($(this).is(':checked')) {
-			$('#text-trans-AltAltJPY').prop("disabled", false);
-			$('#btn-trans-getcurrent').prop("disabled", false);
-		} else {
-			$('#text-trans-AltAltJPY').prop("disabled", true);
-			$('#btn-trans-getcurrent').prop("disabled", true);
-		}
-	});
-	
-	$('#btn-trans-getcurrent').on('click', function () {
-		// todo: 現在のJPY相当額をhttps://coinmarketcap.com/ から取得してtext-trans-AltAltJPYに設定する
-	});
+  $('#flag-trans-isAltAlt').on('change', function () {
+    if ($(this).is(':checked')) {
+      $('#text-trans-AltAltJPY').prop("disabled", false);
+      $('#btn-trans-getcurrent').prop("disabled", false);
+    } else {
+      $('#text-trans-AltAltJPY').prop("disabled", true);
+      $('#btn-trans-getcurrent').prop("disabled", true);
+    }
+  });
 
-	$('#btn-transaction-submit').on('click', function () {
-		addTransaction();
-		calcHistory();
-		$('#btn-update-history').click();
-	});
-	
-	$('#btn-transaction-clear').on('click', function () {
-		clearTransaction();
-	});
+  $('#btn-trans-getcurrent').on('click', function () {
+    // todo: 現在のJPY相当額をhttps://coinmarketcap.com/ から取得してtext-trans-AltAltJPYに設定する
+  });
+
+  $('#btn-transaction-submit').on('click', function () {
+    addTransaction();
+    calcHistory();
+    $('#btn-update-history').click();
+  });
+
+  $('#btn-transaction-clear').on('click', function () {
+    clearTransaction();
+  });
 });
 
 
 /*** その他 ***/
 /* カレンダー用 */
 $(function () {
-	// $.datetimepicker.setLocale('ja');	// todo: error occurs...
-	$('#datetime-profit-from').datetimepicker();
-	$('#datetime-profit-to').datetimepicker();
-	$('#datetime-trans').datetimepicker();
+  // $.datetimepicker.setLocale('ja');  // todo: error occurs...
+  $('#datetime-profit-from').datetimepicker();
+  $('#datetime-profit-to').datetimepicker();
+  $('#datetime-trans').datetimepicker();
 });
